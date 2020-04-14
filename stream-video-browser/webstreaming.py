0f45fbedefb27c5c424e010c30601a25f73ecd58
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2020-04-14 22:16:55 +0700

    UPDATE 14/04/2020
    intent

--- stream-video-browser/webstreaming.py
+++ stream-video-browser/webstreaming.py
@@ -17,8 +17,8 @@ import os
 import datetime
 from pathlib import Path
 import arrow
-
-# filesPath = r"/home/pupan/Documents/Savecamera"
+from flask_cors import CORS
+# filesPath = r"/var/www/html/image/"
 
 # criticalTime = arrow.now().shift(hours=+7).shift(days=-1)
 
@@ -26,9 +26,9 @@ import arrow
 #     if item.is_file():
 #         print (str(item.absolute()))
 #         itemTime = arrow.get(item.stat().st_mtime)
-# 			# if itemTime < criticalTime:
-# 			# 	#remove it
-# 			# pass
+#           # if itemTime < criticalTime:
+#           #   #remove it
+#           # pass
 
 
 
@@ -51,155 +51,175 @@ lock = threading.Lock()
 # initialize a flask object
 app = Flask(__name__)
 
+
+CORS(app)
+cors = CORS(app, resources={
+    
+    r"/*":{
+        "origins":"*"
+        
+        }
+    })
+
+
+
 # initialize the video stream and allow the camera sensor to
 # warmup
 #vs = VideoStream(usePiCamera=1).start()
-vs = VideoStream(src=0).start()
+vs = VideoStream(src=-1).start()
 time.sleep(2.0)
 
 
 
 
 def delete1():
-    	
-		path = r"/home/pupan/Documents/Savecamera"
-		now = time.time()
-		items = os.listdir("/home/pupan/Documents/Savecamera/")
-		newlist = []
-		for names in items:
-    			if names.endswith(".png"):
-    					os.remove(path+'/'+names)
+        
+        path = r"/var/www/html/image/"
+        now = time.time()
+        items = os.listdir("/var/www/html/image/")
+        newlist = []
+        for names in items:
+                if names.endswith(".png"):
+                        os.remove(path+'/'+names)
 
-
+@app.route("/")
+def home_page():
+    return Response(dumps({
+        'content': 'Allow Access Origin'
+        }), mimetype='text/json')
 
 @app.route("/del")
 def delete():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(delete1(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(delete1(),
+        mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 @app.route("/camera")
 def index():
-	# return the rendered template
-	return render_template("index.html")
+    # return the rendered template
+    return render_template("index.html")
 def generate1():
-	cv2.imwrite('/home/pupan/Documents/Savecamera/'+str(datetime.datetime.now())+'.png', outputFrame)
+    return str(datetime.datetime.now())
+    #cv2.imwrite('/var/www/html/image/'+str(datetime.datetime.now())+'.png', outputFrame)
+
 @app.route("/cap")
 def capture():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate1(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+    # return the response generated along with the specific media
+    # type (mime type)
+    date1= str(datetime.datetime.now())
+    cv2.imwrite('/var/www/html/image/'+str(date1)+'.png', outputFrame)
+    return str(date1)
+       #mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 def detect_motion(frameCount):
-	# grab global references to the video stream, output frame, and
-	# lock variables
-	global vs, outputFrame, lock
+    # grab global references to the video stream, output frame, and
+    # lock variables
+    global vs, outputFrame, lock
 
-	# initialize the motion detector and the total number of frames
-	# read thus far
-	md = SingleMotionDetector(accumWeight=0.1)
-	total = 0
+    # initialize the motion detector and the total number of frames
+    # read thus far
+    md = SingleMotionDetector(accumWeight=0.1)
+    total = 0
 
-	# loop over frames from the video stream
-	while True:
-		# read the next frame from the video stream, resize it,
-		# convert the frame to grayscale, and blur it
-		frame = vs.read()
-		frame = imutils.resize(frame, width=1000)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7, 7), 0)
-		
+    # loop over frames from the video stream
+    while True:
+        # read the next frame from the video stream, resize it,
+        # convert the frame to grayscale, and blur it
+        frame = vs.read()
+        frame = imutils.resize(frame, width=500)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        
 
 
-		# grab the current timestamp and draw it on the frame
-		timestamp = datetime.datetime.now()
-		cv2.putText(frame, timestamp.strftime(
-			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-		#cv2.imwrite('/home/pupan/Documents/'+str(datetime.datetime.now())+'.png', outputFrame)
-		
-		# if the total number of frames has reached a sufficient
-		# number to construct a reasonable background model, then
-		# continue to process the frame
-		
+        # grab the current timestamp and draw it on the frame
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime(
+            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        #cv2.imwrite('/home/pupan/Documents/'+str(datetime.datetime.now())+'.png', outputFrame)
+        
+        # if the total number of frames has reached a sufficient
+        # number to construct a reasonable background model, then
+        # continue to process the frame
+        
 
-		if total > frameCount:
-			# detect motion in the image
-			motion = md.detect(gray)
+        if total > frameCount:
+            # detect motion in the image
+            motion = md.detect(gray)
 
-			# cehck to see if motion was found in the frame
-			if motion is not None:
-				# unpack the tuple and draw the box surrounding the
-				# "motion area" on the output frame
-				(thresh, (minX, minY, maxX, maxY)) = motion
-				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-					(0, 0, 255), 2)
-		
-		# update the background model and increment the total number
-		# of frames read thus far
-		md.update(gray)
-		total += 1
+            # cehck to see if motion was found in the frame
+            if motion is not None:
+                # unpack the tuple and draw the box surrounding the
+                # "motion area" on the output frame
+                (thresh, (minX, minY, maxX, maxY)) = motion
+                cv2.rectangle(frame, (minX, minY), (maxX, maxY),
+                    (0, 0, 255), 2)
+        
+        # update the background model and increment the total number
+        # of frames read thus far
+        md.update(gray)
+        total += 1
 
-		# acquire the lock, set the output frame, and release the
-		# lock
-		with lock:
-			outputFrame = frame.copy()
+        # acquire the lock, set the output frame, and release the
+        # lock
+        with lock:
+            outputFrame = frame.copy()
 
 def generate():
-	# grab global references to the output frame and lock variables
-	global outputFrame, lock
+    # grab global references to the output frame and lock variables
+    global outputFrame, lock
 
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
 
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-			
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            
 
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
 
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
+        # yield the output frame in the byte format
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+            bytearray(encodedImage) + b'\r\n')
 
 @app.route("/video_feed")
 def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(),
+        mimetype = "multipart/x-mixed-replace; boundary=frame")
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
-	# construct the argument parser and parse command line arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
-	args = vars(ap.parse_args())
+    # construct the argument parser and parse command line arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--ip", type=str, required=True,
+        help="ip address of the device")
+    ap.add_argument("-o", "--port", type=int, required=True,
+        help="ephemeral port number of the server (1024 to 65535)")
+    ap.add_argument("-f", "--frame-count", type=int, default=32,
+        help="# of frames used to construct the background model")
+    args = vars(ap.parse_args())
 
-	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(
-		args["frame_count"],))
-	t.daemon = True
-	t.start()
+    # start a thread that will perform motion detection
+    t = threading.Thread(target=detect_motion, args=(
+        args["frame_count"],))
+    t.daemon = True
+    t.start()
 
-	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
+    # start the flask app
+    app.run(host=args["ip"], port=args["port"], debug=True,
+        threaded=True, use_reloader=False)
 
 # release the video stream pointer
 vs.stop()
